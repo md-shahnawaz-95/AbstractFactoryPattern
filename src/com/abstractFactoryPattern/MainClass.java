a9bf7922b210a76230e236f90708106b7af4df03
Md Shahnawaz <mdshahnawaz95@outlook.com>
2021-06-28 23:38:57 +0530

    adding initial file for Abstract Factory Pattern

---
+++ src/com/abstractFactoryPattern/MainClass.java
@@ -0,0 +1,7 @@
+package src.com.abstractFactoryPattern;
+
+public class MainClass {
+    public static void main(String args[]){
+        System.out.println("This is a Abstract Factory Pattern.");
+    }
+}
